08b3b859e66dcd21dc624fbb3c2a6f42c85478b5
Nikil Kumar <kumarnikil220@gmail.com>
2024-07-18 23:01:51 +0530

    Hello from spring boot application

---
+++ example1/src/main/java/com/eazybytes/eazyschool/EazySchoolApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.eazyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazySchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazySchoolApplication.class, args);
+	}
+
+}
