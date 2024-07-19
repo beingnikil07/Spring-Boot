66dabc58f27da31b235c2d465d3aee8c906a1ceb
Nikil Kumar <kumarnikil220@gmail.com>
2024-07-20 01:02:55 +0530

    building dynamic content using thymeleaf

--- example2/src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
+++ example2/src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
@@ -1,16 +1,19 @@
 package com.eazybytes.eazyschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 // Controller annotation indicates that a particular class serves the role of a controller
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String displayHomePage(){
+    @RequestMapping(value = {"","/","Home"})
+    //Model is an interface present in spring mvc framework which act as a container between your UI and backend code
+    public String displayHomePage(Model model){
+        model.addAttribute("username","Nikhil");
         return "Home.html";
     }
+
 }
