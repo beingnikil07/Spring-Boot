3b4f84a20e0747ac283fcd73ce69b7227050fad3
Nikil Kumar <kumarnikil220@gmail.com>
2024-07-21 00:41:46 +0530

    Building home page of the UI

---
+++ example4/src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.eazybytes.eazyschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// Controller annotation indicates that a particular class serves the role of a controller
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"","/","Home"})
+    //Model is an interface present in spring mvc framework which act as a container between your UI and backend code
+    public String displayHomePage(Model model){
+        model.addAttribute("username","nikil");
+        return "Home.html";
+    }
+
+}
