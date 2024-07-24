d55b8018cb20c0c62cb280919256db41db793d58
Nikil Kumar <kumarnikil220@gmail.com>
2024-07-24 23:52:12 +0530

    Submit information from Contact page using POJO object

---
+++ example6/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -0,0 +1,28 @@
+package com.eazybytes.eazyschool.service;
+
+import com.eazybytes.eazyschool.model.Contact;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactService {
+
+    private static Logger log= LoggerFactory.getLogger(ContactService.class);
+
+
+
+    /**
+     * Save Contact Details into DB
+     * @param contact
+     * @return boolean
+     */
+
+    public  static  boolean saveMessageDetails(Contact contact){
+        boolean isSaved=true;
+
+        //TODO:Need to persist the data into the Db table,but as of now we are just logging it to the console
+        log.info(contact.toString());
+        return isSaved;
+    }
+}
