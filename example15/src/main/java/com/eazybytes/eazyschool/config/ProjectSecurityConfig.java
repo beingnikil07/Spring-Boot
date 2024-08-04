2a0b06d3343b3428cfcf84e909f847bd9c916e9a
Nikil Kumar <kumarnikil220@gmail.com>
2024-08-04 15:39:00 +0530

    configure denyAll requests to access the web application

--- example15/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example15/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.eazybytes.eazyschool.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,12 +16,7 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-//                .anyRequest().permitAll()                        //permit all requests inside the web application
-//                .and().formLogin()
-//                .and().httpBasic();
-
-                //Deny all requests inside the web application
-                .anyRequest().denyAll()
+                .anyRequest().permitAll()                        //permit all requests inside the web application
                 .and().formLogin()
                 .and().httpBasic();
 
