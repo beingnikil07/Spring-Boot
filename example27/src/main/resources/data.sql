2d49ce85aacbd81e0f7bc86cb7f3cba0375dcc76
Nikil Kumar <kumarnikil220@gmail.com>
2024-08-15 16:21:01 +0530

    saving holidays to h2 database and displaying holidays from h2 database

---
+++ example27/src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Jan 1 ','New Year''s Day','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Oct 29 ','Raksha Bandhan','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Nov 24 ','Thanksgiving Day','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Dec 25 ','Christmas','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Oct 2 ','Mahatma Gandhi Day','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Aug 15 ','Independence Day','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Jan 26 ','Republic Day','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+  VALUES (' May 8 ','Guru Rabindranath Jayanti ','FEDERAL',CURDATE(),'DBA');
