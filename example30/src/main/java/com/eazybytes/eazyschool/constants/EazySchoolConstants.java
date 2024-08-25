82dd198bcc907bff056c977a8837554faa3fdffb
Nikil Kumar <kumarnikil220@gmail.com>
2024-08-26 04:50:39 +0530

    implementing automatic auditing in my application

---
+++ example30/src/main/java/com/eazybytes/eazyschool/constants/EazySchoolConstants.java
@@ -0,0 +1,6 @@
+package com.eazybytes.eazyschool.constants;
+
+public interface EazySchoolConstants {
+    public static final String OPEN = "Open";
+    public static final String CLOSE = "Close";
+}
