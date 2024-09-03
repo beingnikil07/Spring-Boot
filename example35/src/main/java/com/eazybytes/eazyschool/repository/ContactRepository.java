baaceca88c830439af67220b27c4a7f19d3aab68
Nikil Kumar <kumarnikil220@gmail.com>
2024-09-03 23:06:52 +0530

    displaying profile link in dashboard section

---
+++ example35/src/main/java/com/eazybytes/eazyschool/repository/ContactRepository.java
@@ -0,0 +1,13 @@
+package com.eazybytes.eazyschool.repository;
+import com.eazybytes.eazyschool.model.Contact;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface ContactRepository extends CrudRepository<Contact,Integer> {
+    //abstract method
+    List<Contact> findByStatus(String status);
+}
---
+++ example35/src/main/resources/eazyschool_data.sql
@@ -0,0 +1,33 @@
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Jan 1 ','New Year''s Day','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Oct 31 ','Halloween','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Nov 24 ','Thanksgiving Day','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Dec 25 ','Christmas','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Jan 17 ','Martin Luther King Jr. Day','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' July 4 ','Independence Day','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Sep 5 ','Labor Day','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+  VALUES (' Nov 11 ','Veterans Day','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `roles` (`role_name`,`created_at`, `created_by`)
+  VALUES ('ADMIN',CURDATE(),'DBA');
+
+INSERT INTO `roles` (`role_name`,`created_at`, `created_by`)
+  VALUES ('STUDENT',CURDATE(),'DBA');
+
+
+INSERT INTO `person` (`name`,`email`,`mobile_number`,`pwd`,`role_id`,`created_at`, `created_by`)
+  VALUES ('Admin','admin@eazyschool.com','6395964648','$2a$12$bYrKg7E8Z7Fj6WmfUS74nOMPhJmN33O1i04H.FbWcS8BiElXlFWhK', 1 ,CURDATE(),'DBA');
