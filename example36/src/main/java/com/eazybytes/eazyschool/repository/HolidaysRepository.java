98fc3d0f24d9ac0346b514e683a4c6582a54b625
Nikil Kumar <kumarnikil220@gmail.com>
2024-09-06 15:42:40 +0530

    add some buttons to admin dashboard

---
+++ example36/src/main/java/com/eazybytes/eazyschool/repository/HolidaysRepository.java
@@ -0,0 +1,11 @@
+package com.eazybytes.eazyschool.repository;
+import com.eazybytes.eazyschool.model.Holiday;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface HolidaysRepository extends CrudRepository<Holiday,String> {
+
+
+}
