8b15f92fc1530f5958e5299973f671019ac86b54
Nikil Kumar <kumarnikil220@gmail.com>
2024-09-07 22:34:24 +0530

    Now admin can add a new class

--- example36/src/main/java/com/eazybytes/eazyschool/controller/AdminController.java
+++ example36/src/main/java/com/eazybytes/eazyschool/controller/AdminController.java
@@ -2,22 +2,66 @@ package com.eazybytes.eazyschool.controller;
 
 
 import com.eazybytes.eazyschool.model.EazyClass;
+import com.eazybytes.eazyschool.model.Person;
+import com.eazybytes.eazyschool.repository.EazyClassRepository;
+import com.eazybytes.eazyschool.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
 @RequestMapping("admin")
 public class AdminController {
 
+    @Autowired
+    EazyClassRepository eazyClassRepository;
+
+    @Autowired
+    PersonRepository personRepository;
+
+
     @RequestMapping("/displayClasses")
     public ModelAndView displayClasses(Model model){
+        List<EazyClass> eazyClasses=eazyClassRepository.findAll();
         ModelAndView modelAndView=new ModelAndView("classes.html");
+        modelAndView.addObject("eazyClasses",eazyClasses);
         modelAndView.addObject("eazyClass",new EazyClass());
         return  modelAndView;
     }
+
+
+    @PostMapping("/addNewClass")
+    public ModelAndView addNewClass(Model model, @ModelAttribute("eazyClass") EazyClass eazyClass){
+        eazyClassRepository.save(eazyClass);
+        ModelAndView modelAndView=new ModelAndView("redirect:/admin/displayClasses");
+        return  modelAndView;
+    }
+
+    @RequestMapping("/deleteClass")
+    public ModelAndView deleteClass(Model model, @RequestParam int id) {
+        Optional<EazyClass> eazyClass = eazyClassRepository.findById(id);
+        for(Person person : eazyClass.get().getPersons()){
+            person.setEazyClass(null);
+            personRepository.save(person);
+        }
+        eazyClassRepository.deleteById(id);
+        ModelAndView modelAndView = new ModelAndView("redirect:/admin/displayClasses");
+        return modelAndView;
+    }
+
+    @GetMapping("/displayStudents")
+    public ModelAndView displayStudents(Model model,@RequestParam int classId){
+        ModelAndView modelAndView=new ModelAndView("students.html");
+        return  modelAndView;
+    }
+
+
 
 }
