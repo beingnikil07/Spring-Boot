585a47cd1d79d545057932d778f68fd8d60cfd02
Nikil Kumar <kumarnikil220@gmail.com>
2024-09-24 15:54:39 +0530

    Demo of delete operation using rest service and RequestEntity

--- example39/src/main/java/com/eazybytes/eazyschool/rest/ContactRestController.java
+++ example39/src/main/java/com/eazybytes/eazyschool/rest/ContactRestController.java
@@ -6,12 +6,15 @@ import com.eazybytes.eazyschool.repository.ContactRepository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -52,6 +55,24 @@ public class ContactRestController {
                 .header("isMsgSaved", "true")
                 .body(response);
     }
+
+    @DeleteMapping("/deleteMsg")
+    public ResponseEntity<Response> deleteMsg(RequestEntity<Contact> requestEntity){
+        HttpHeaders headers = requestEntity.getHeaders();
+        headers.forEach((key, value) -> {
+            log.info(String.format(
+                    "Header '%s' = %s", key, value.stream().collect(Collectors.joining("|"))));
+        });
+        Contact contact = requestEntity.getBody();
+        contactRepository.deleteById(contact.getContactId());
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message successfully deleted");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
+
 
 
 }
