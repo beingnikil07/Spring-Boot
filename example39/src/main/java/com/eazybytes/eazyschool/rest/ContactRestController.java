dbc4995cac73c06a5843cf08b28283fc5976cd73
Nikil Kumar <kumarnikil220@gmail.com>
2024-09-25 11:08:27 +0530

    Sending response in XML format in rest services

--- example39/src/main/java/com/eazybytes/eazyschool/rest/ContactRestController.java
+++ example39/src/main/java/com/eazybytes/eazyschool/rest/ContactRestController.java
@@ -7,10 +7,7 @@ import com.eazybytes.eazyschool.repository.ContactRepository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
@@ -19,7 +16,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
-@RequestMapping(path = "/api/contact")
+@RequestMapping(path = "/api/contact",produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
 @CrossOrigin("*")  //allow for any domain
 public class ContactRestController {
 
