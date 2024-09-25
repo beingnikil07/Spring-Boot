875165038f4577fcd9255e5ea67948f73c8f731b
Nikil Kumar <kumarnikil220@gmail.com>
2024-09-25 11:30:18 +0530

    Demo of content filter in rest services using @JsonIgnore annotation of jackson library

--- example39/src/main/java/com/eazybytes/eazyschool/model/BaseEntity.java
+++ example39/src/main/java/com/eazybytes/eazyschool/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.eazybytes.eazyschool.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -12,20 +13,30 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+/*
+The @JsonIgnore annotation in Jackson (a popular Java library used for processing JSON data) is used to
+instruct the serialization process to ignore specific fields or properties when converting a Java object to JSON.
+This means that fields annotated with @JsonIgnore will not be included in the generated JSON output.
+ */
+
 @Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @CreatedDate
     @Column(updatable = false)
+    @JsonIgnore
     private LocalDateTime createdAt;
     @CreatedBy
     @Column(updatable = false)
+    @JsonIgnore
     private  String createdBy;
     @LastModifiedDate
     @Column(insertable = false)
+    @JsonIgnore
     private LocalDateTime updatedAt;
     @LastModifiedBy
     @Column(insertable = false)
+    @JsonIgnore
     private  String updatedBy;
 }
