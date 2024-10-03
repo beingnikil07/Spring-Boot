34a296c96af6c91fffb5d091d60df55fe84dc2ee
Nikil Kumar <kumarnikil220@gmail.com>
2024-10-03 23:37:41 +0530

    Demo of Types of logging

--- example42/src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
+++ example42/src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
@@ -31,10 +31,10 @@ public class DashboardController {
 
     private  void logMessages(){
         log.error("Error message from the Dashboard page ");
-        log.error(" Warning message from the Dashboard page ");
-        log.error("Info message from the Dashboard page  ");
-        log.error("Debug message from the Dashboard page  ");
-        log.error("Trace message from the Dashboard page ");
+        log.warn(" Warning message from the Dashboard page ");
+        log.info("Info message from the Dashboard page  ");
+        log.debug("Debug message from the Dashboard page  ");
+        log.trace("Trace message from the Dashboard page ");
     }
 
 }
